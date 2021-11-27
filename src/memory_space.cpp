5f81c46c3a82e20df8957cdfac46571e9a129359
Declan <declan@ortinau.com>
2021-11-26 21:28:17 -0600

    a few more function implementations

--- src/memory_space.cpp
+++ src/memory_space.cpp
@@ -40,16 +40,47 @@ void MemorySpace::updateTime(const float newTime)
 
 vector<Hole> MemorySpace::getHoles()
 {
-  return vector<Hole>();
+  vector<Hole> Holes;
+  int holeLocation, holeSize;
+  for (int i = 0; i < space; i++)
+  {
+    if(memory[i] == 0)
+    {
+      holeLocation = i;
+      holeSize = 1;
+      // continue iterating until we reach a 1 to find size
+      while(memory[i] != 1 && i < space)
+      {
+        i++;
+        holeSize++;
+      }
+      Holes.push_back(Hole(holeLocation,holeSize));
+    }
+  }
+  return Holes;
 }
 
 float MemorySpace::timeUntilNextFinishes()
 {
-  return 0;
+  // preliminarily check to make sure there are processes running
+  if(activeProcesses.size() == 0)
+    return -1;
+
+  float minTimeLeft = 99999; // a number theoretically bigger than any input duration
+  for(auto& [p, duration, location] : activeProcesses)
+  {
+    if(duration < minTimeLeft)
+      minTimeLeft = duration;
+  }
+  return minTimeLeft;
 }
 
 int MemorySpace::usage()
 {
-  return 0;
+  int used = 0;
+  for (int i = 0; i < space; i++)
+    if (memory[i] == 1)
+      used++;
+  return used;
 }
 
